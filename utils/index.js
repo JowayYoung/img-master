713003084f285fe4f64fe8c64ee533d65439572a
N7440 <yangzewei@corp.netease.com>
2018-11-08 10:37:20 +0800

    update

--- utils/index.js
+++ utils/index.js
@@ -8,24 +8,6 @@ const readDir = promisify(fs.readdir);
 const readStat = promisify(fs.stat);
 const removeDir = promisify(rimraf);
 
-function authType(val, type) {
-	const target = formatAnswer(val);
-	if (!target) {
-		return chalk.white.bgRed("Only input number and comma");
-	}
-	if (type === "resize") {
-		return true;
-	}
-	if (type === "crop") {
-		const size = target[0] && target[1];
-		return size ? true : chalk.white.bgRed("Width and height can only input positive number");
-	}
-	if (type === "compress") {
-		const quality = target[0];
-		return quality > 0 && quality <= 100 ? true : chalk.white.bgRed("Quality can only input positive number between 0 and 100");
-	}
-}
-
 function formatAnswer(val) {
 	const reg = new RegExp(/^[0-9,]+$/);
 	const flag = reg.test(val);
@@ -74,11 +56,29 @@ async function removeDist() {
 	console.log(chalk.white.bgGreen("\nThe output directory was cleared successfully\n"));
 }
 
+function validType(val, type) {
+	const target = formatAnswer(val);
+	if (!target) {
+		return chalk.white.bgRed("Only input number and comma");
+	}
+	if (type === "resize") {
+		return true;
+	}
+	if (type === "crop") {
+		const size = target[0] && target[1];
+		return size ? true : chalk.white.bgRed("Width and height can only input positive number");
+	}
+	if (type === "compress") {
+		const quality = target[0];
+		return quality > 0 && quality <= 100 ? true : chalk.white.bgRed("Quality can only input positive number between 0 and 100");
+	}
+}
+
 module.exports = {
-	authType,
 	formatAnswer,
 	getFileName,
 	help,
 	mapFile,
-	removeDist
+	removeDist,
+	validType
 };
